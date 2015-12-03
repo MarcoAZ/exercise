535e2db110e1150c37338e3206d025dd18d29487
Marco Zamora <marco.zamora@comcast.net>
2015-12-02 23:19:59 -0600

    debug 16

--- public/scripts.js
+++ public/scripts.js
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 
 	function bindButtons(){
 		document.getElementById("add").addEventListener('click', function(e){
-			var req = new XMLHttpRequest();
+			//var req = new XMLHttpRequest();
 			
 			//build a payload
 			var payload = {name:null, reps:null, weight:null, date:null, lbs:null};
-			// payload.name = document.getElementById('name').value;
-			// payload.reps =  document.getElementById('reps').value;
-			// payload.weight =  document.getElementById('weight').value;
-			// payload.date =  document.getElementById('date').value;
-			// payload.lbs =  document.getElementById('lbs').value;
-			
+			payload.name = document.getElementById('name').value;
+			payload.reps =  document.getElementById('reps').value;
+			payload.weight =  document.getElementById('weight').value;
+			payload.date =  document.getElementById('date').value;
+			payload.lbs =  document.getElementById('lbs').value;
+			console.log(JSON.stringify(payload));
 			// req.open('GET', 'http://54.149.55.4:3000/insert', true);
 			// req.setRequestHeader('Content-Type', 'application/json');
 			// req.addEventListener('load',function(){
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 			  // } else {
 				// console.log("Error in network request: " + req.statusText);
 			  // }});
-			console.log(JSON.stringify(payload));
+			
 			//req.send(JSON.stringify(payload));
 			e.preventDefault();
 	})
