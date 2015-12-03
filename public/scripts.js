39ef8cd026bece5604b58dbf403132a9ec1e8e2d
Marco Zamora <marco.zamora@comcast.net>
2015-12-02 22:27:43 -0600

    debug 5

--- public/scripts.js
+++ public/scripts.js
@@ -13,15 +13,15 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 			payload.lbs =  document.getElementById('lbs').value;
 			console.log("payload: " + payload.name);
 			
-			req.open('GET', 'http://54.149.55.4:3000/insert', true);
-			//req.setRequestHeader('Content-Type', 'application/json');
-			req.addEventListener('load',function(){
-			  if(req.status >= 200 && req.status < 400){
-				var response = JSON.parse(req.responseText);
-				console.log(response.insert);
-			  } else {
-				console.log("Error in network request: " + req.statusText);
-			  }});
+			// req.open('GET', 'http://54.149.55.4:3000/insert', true);
+			// //req.setRequestHeader('Content-Type', 'application/json');
+			// req.addEventListener('load',function(){
+			  // if(req.status >= 200 && req.status < 400){
+				// var response = JSON.parse(req.responseText);
+				// console.log(response.insert);
+			  // } else {
+				// console.log("Error in network request: " + req.statusText);
+			  // }});
 			req.send(payload);
 			e.preventDefault();
 	})
