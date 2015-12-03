62cf0d485835236bab1ced6ddb687d2f2cc4434c
Marco Zamora <marco.zamora@comcast.net>
2015-12-02 20:21:32 -0600

    set up
    setting up folder structure, main page *fingers crossed*

---
+++ exercise.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  host  : 'localhost',
+  user  : 'student',
+  password: 'default',
+  database: 'student'
+});
+
+var express = require('express');
+var app = express();
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+
+app.get('/',function(req,res,next){
+  var context = {};
+  mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = JSON.stringify(rows);
+    res.render('home', context);
+  });
+});
