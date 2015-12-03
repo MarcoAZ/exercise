7375670f5ea16d467938aeea3615aa5931f89189
Marco Zamora <marco.zamora@comcast.net>
2015-12-02 20:28:54 -0600

    reset-table

--- exercise.js
+++ exercise.js
@@ -29,6 +29,23 @@ app.get('/',function(req,res,next){
   });
 });
 
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  pool.query("DROP TABLE IF EXISTS todo", function(err){
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    mysql.pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
